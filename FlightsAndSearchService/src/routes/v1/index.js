52813aeef9825d26b78df91a7b2b7d30d07bbbf9
Punya Jain <punyajain06@gmail.com>
2024-10-06 05:04:43 +0530

    added routes for flight

--- FlightsAndSearchService/src/routes/v1/index.js
+++ FlightsAndSearchService/src/routes/v1/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const router = express.Router();
+const { FlightMiddlewares } = require('../../middlewares/index');
 
 const cityController = require('../../controllers/city-controller');
+const FlightController = require('../../controllers/flight-controller');
 
 router.post('/city', cityController.create);
 router.delete('/city/:id', cityController.destroy);
 router.get('/city/:id', cityController.get);
 router.get('/city', cityController.getAll);
 router.patch('/city/:id', cityController.update);
+
+router.post(
+    '/flights', 
+    FlightMiddlewares.validateCreateFlight, 
+    FlightController.create
+);
+router.get('/flights', FlightController.getAll);
+router.get('/flights/:id', FlightController.get);
+router.patch('/flights/:id', FlightController.update);
 
 module.exports = router;
