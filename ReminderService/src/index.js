4177f8de52da8736c8ed0b011d255aeada27b1a6
Punya Jain <punyajain06@gmail.com>
2024-10-14 11:48:58 +0530

    setup SMTP protocol in reminderService

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
+const { sendBasicEmail } = require('./services/email-service')
 
 const setupAndStartServer = async () => {
     const app = express();
@@ -11,6 +12,12 @@ const setupAndStartServer = async () => {
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
         
+        sendBasicEmail(
+            'punyajain_ae21b14_77@dtu.ac.in',
+            'punyajain06@gmail.com',
+            'This is a testing mail',
+            'Hey, how are you I hope this email finds you well!!'
+        );
     });
 }
 
