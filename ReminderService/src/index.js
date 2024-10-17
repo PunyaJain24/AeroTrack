6268a3d936f68d5e01d61d6b70c4dd69bc10a79f
Punya Jain <punyajain06@gmail.com>
2024-10-17 14:59:43 +0530

    Adding message Queue subscription

--- ReminderService/src/index.js
+++ ReminderService/src/index.js
@@ -2,24 +2,28 @@ const express = require('express');
 const bodyParser = require('body-parser');
 
 const { PORT } = require('./config/serverConfig');
-const { sendBasicEmail } = require('./services/email-service')
-const jobs = require('./utils/job');
+
 const TicketController = require('./controllers/ticket-controller');
+const EmailService = require('./services/email-service');
+
+const jobs = require('./utils/job');
+const { subscribeMessage, createChannel } = require('./utils/messageQueue');
+const { REMINDER_BINDING_KEY } = require('./config/serverConfig');
 
 const setupAndStartServer = async () => {
     const app = express();
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({extended: true}));
+
     app.post('/api/v1/tickets', TicketController.create);
+
+    const channel = await createChannel();
+    subscribeMessage(channel, EmailService.subscribeEvents, REMINDER_BINDING_KEY);
+
     app.listen(PORT, () => {
         console.log(`Server started at port ${PORT}`);
-        jobs();
-        // sendBasicEmail(
-        //     'punyajain_ae21b14_77@dtu.ac.in',
-        //     'punyajain06@gmail.com',
-        //     'This is a testing mail',
-        //     'Hey, how are you I hope this email finds you well!!'
-        // );
+        // jobs();
+        
     });
 }
 
--- ReminderService/src/services/email-service.js
+++ ReminderService/src/services/email-service.js
@@ -46,9 +46,34 @@ const createNotification = async (data) => {
     }
 }
 
+const subscribeEvents = async (payload) => {
+    let service = payload.service;
+    let data = payload.data;
+    switch(service) {
+        case 'CREATE_TICKET':
+            await createNotification(data);
+            break;
+        case 'SEND_BASIC_MAIL':
+            await sendBasicEmail(data);
+            break;
+        default: 
+            console.log('No valid event received');
+            break;
+    }
+}
+
 module.exports = {
     sendBasicEmail,
     fetchPendingEmails,
     createNotification,
     updateTicket,
+    subscribeEvents
 }
+
+
+/**
+ * SMTP -> a@b.com
+ * receiver-> d@e.com
+ * 
+ * from: support@noti.com
+ */
